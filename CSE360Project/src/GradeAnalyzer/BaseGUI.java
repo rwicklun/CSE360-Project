e1e138fdc61b346ba7fcf58d1b1110724a62f951
rwicklun <rwicklun@asu.edu>
2019-06-21 17:15:59 -0700

    This is the initial commit of the Grade Analyzer, makes a basic interface.

---
+++ CSE360Project/src/GradeAnalyzer/BaseGUI.java
@@ -0,0 +1,146 @@
+package GradeAnalyzer;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+@SuppressWarnings("serial")
+public class BaseGUI extends JFrame { 
+	
+	
+	public BaseGUI() {
+		setBackground(Color.BLACK);
+		UIManager.put("TabbedPane.selected", Color.blue);
+		GUIComponents();
+        int screenWidth = (int) screenSize.getWidth();
+        int screenHeight = (int) screenSize.getHeight();
+        this.setSize(screenWidth/2, screenHeight/2);
+        this.setResizable(false);
+        this.setLocation((screenWidth - this.getSize().width) / 2,(screenHeight - this.getSize().height) / 2); 
+        
+	}
+
+	public static void main(String[] args) {
+		new BaseGUI().setVisible(true);
+		
+	}
+	private void GUIComponents() {
+		getContentPane().setLayout(new BorderLayout());
+		getContentPane().setBackground(new Color( 200, 200, 200));//left side bar color
+        setTitle("Grade Analyzer");
+		//base tabs and such
+		basePanel = new JTabbedPane();
+		basePanel.setFont(new Font("Tahoma", Font.PLAIN, 18));//tab font and size
+        basePanel.setTabPlacement(JTabbedPane.LEFT);
+        basePanel.setBackground(Color.BLACK);//tab background color
+        basePanel.setForeground(Color.WHITE);//tab text color
+        getContentPane().add(basePanel, BorderLayout.CENTER);
+        
+        //Home page tab start
+        mainPanel = new JPanel(new BorderLayout());
+        mainPanel.setForeground(Color.BLACK);
+        mainPanel.setBorder(BorderFactory.createMatteBorder(3, 3, 3, 3, Color.BLACK));// Home page border color/size
+        
+        mainDescription = new JTextArea();
+        mainDescription.setLineWrap(true);
+        mainDescription.setWrapStyleWord(true);
+        mainDescription.setFont(new Font("Times New Roman", Font.PLAIN, 18));// description text style
+        mainDescription.setEditable(false);
+        mainDescription.setBackground(Color.white);// the text background is white
+        // start of text for home description
+        	mainDescription.setText("Change to description of the project.");
+        // end of text for home description
+        scrollPane = new JScrollPane(mainDescription);
+        mainPanel.add(scrollPane);
+        mainPanel.setBackground(Color.white);
+        basePanel.addTab("Home", mainPanel);
+        basePanel.setForegroundAt(0, Color.WHITE);
+		//home page tab end
+		
+		//load tab start
+        loadButton = new JButton("Load Tab");
+        loadButton.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		basePanel.addTab("Load a File", loadButton);
+		//load tab end
+		
+		//stats tab start
+        statsButton = new JButton("Stats Tab");
+		basePanel.addTab("Grade Statistics", statsButton);
+		//stats tab end
+		
+		//percentiles tab start
+        percentilesButton = new JButton("Percentiles Tab");
+		basePanel.addTab("Grade Percentiles", percentilesButton);
+		//percentiles tab end
+		
+		//user tab start
+        userButton = new JButton("User Tab");
+		basePanel.addTab("Change Grades", userButton);
+		//user tab end
+		
+        // end of base tabs and such 
+        
+        //bottom buttons and such
+        bottomPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 16, 5));
+        bottomPanel.setBackground(new Color( 200, 200, 200));
+        
+        recordButton = new JButton("Create Record");
+        recordButton.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        recordButton.setForeground(new Color(255, 255, 255));
+        recordButton.setBackground(Color.BLUE);
+        recordButton.addActionListener(new RecordListener());
+        bottomPanel.add(recordButton);
+        
+        saveButton = new JButton("Save");
+        saveButton.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        saveButton.setForeground(new Color(255, 255, 255));
+        saveButton.setBackground(Color.BLUE);
+        saveButton.addActionListener(new SaveListener());
+        bottomPanel.add(saveButton);
+        
+        closeButton = new JButton("Close");
+        closeButton.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        closeButton.setForeground(new Color(255, 255, 255));
+        closeButton.setBackground(Color.BLUE);
+        closeButton.addActionListener(new CloseListener());
+        bottomPanel.add(closeButton);
+        getContentPane().add(bottomPanel, BorderLayout.SOUTH);
+        //end of bottom buttons and such
+        
+        
+        
+		
+	}
+	private class RecordListener implements ActionListener {
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+	        //DOES NOTHING
+	    }
+	}
+	private class SaveListener implements ActionListener {
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+	        //DOES NOTHING
+	    }
+	}
+	private class CloseListener implements ActionListener {
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+	        //exits the program, closing the GUI.
+	        System.exit(0);
+	    }
+	}
+	private JTabbedPane basePanel;
+	private JPanel mainPanel;
+	private JTextArea mainDescription;
+	private JButton loadButton;
+	private JButton percentilesButton;
+	private JButton userButton;
+	private JButton statsButton;
+	private JPanel bottomPanel;
+	private JButton closeButton;
+	private JButton saveButton;
+	private JButton recordButton;
+	static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	private JScrollPane scrollPane;
+
+}
